0e02b0e429e3215734974ca42be7ac3e8bc69c71
Ignacio Calafate <83598755+nachocalafate@users.noreply.github.com>
2025-05-23 17:28:19 -0300

    Delete instalador.bat

--- instalador.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo 🔧 Instalando módulos necesarios...
-
-python -m pip install --upgrade pip
-python -m pip install ultralytics deep_sort_realtime opencv-python matplotlib streamlit
-
-echo ✅ Todos los módulos fueron instalados.
-pause
